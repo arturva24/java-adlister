3fdeed1da1db7ce296da03f901c66b1c64db606f
Art <art_jr2000@yahoo.com>
2017-10-16 16:18:44 -0500

    continued exercise with annotations on Hello World Servlet -added annotations if they choose to use

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,13 +1,22 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet(name = "helloWorld", urlPatterns = "/sayhello")
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        String name = req.getParameter("name");
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
-        out.println("<h1>Hello World!<h1>");
+//        out.println("<h1>Hello World!<h1>");
+
+        if(name == null){
+            out.println("<h2>Hello World!!</h2>");
+        }else {
+            out.println("<h2>Hello " + name + "</h2>");
+        }
     }
 }
