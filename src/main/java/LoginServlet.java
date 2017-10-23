1a6a47502d59674a5784aa52c6f7cc31dc014910
Art <art_jr2000@yahoo.com>
2017-10-23 14:48:10 -0500

    updated classes to show form

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,35 @@
+
+
+
+//loginservlet.java
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getSession().getAttribute("user") == null) { //can't use false, must use null; tests to see if session is null
+            response.sendRedirect("/login.jsp");
+        } else {
+            request.getRequestDispatcher("/profile.jsp").forward(request, response);
+        }
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        boolean validAttempt = username.equals("admin") && password.equals("password");
+        if (validAttempt) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", username); //username not in quotes so we can recognize it
+            response.sendRedirect("/profile");
+        } else {
+            response.sendRedirect("/login");
+        }
+    }
+}
---
+++ src/main/java/ViewProfileServlet.java
@@ -0,0 +1,25 @@
+
+
+//
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+
+@WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
+public class ViewProfileServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //user is logged in, send to profile
+        if (request.getSession().getAttribute("user") == null) { //grab the session from the request; if key not there, value of null
+            response.sendRedirect("/login"); //send redirect to
+        } else {
+            request.getRequestDispatcher("/profile.jsp").forward(request, response);
+        }
+        //commented out and displays NOTHING
+    }
+}
