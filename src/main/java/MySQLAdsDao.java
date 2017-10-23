fbd22644497f42cb29d4d0989e4fb0e2f68ed03f
Art <art_jr2000@yahoo.com>
2017-10-23 15:20:27 -0500

    updated classes to show login

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,59 +1,69 @@
-import com.mysql.jdbc.Driver;
+
+
 import java.sql.*;
-import java.util.List;
+import com.mysql.cj.jdbc.Driver;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MySQLAdsDao implements Ads {
     Connection connection = null;
 
-    public MySQLAdsDao (){
+    public MySQLAdsDao(Config config) {
         try {
             DriverManager.registerDriver(new Driver());
-            Connection connection = DriverManager.getConnection(
-//                    "jdbc:mysql://localhost/movies_db",
-//                    "movies_user",
-//                    "password"
-                    Config.getUrl(),
-                    Config.getUser(),
-                    Config.getPassword()
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
             );
-        } catch (SQLException e) {
-            throw new RuntimeException("Error");
-        }
-    }
 
-    public MySQLAdsDao(Config config) {
+        } catch (SQLException e) {
+            throw new RuntimeException("Error establishing database connection!", e);
+        }
+
     }
 
     @Override
     public List<Ad> all() {
-        return null;
+        String selectQuery = "SELECT * FROM adlister_db.ads";
+        List<Ad> ads = new ArrayList<>();
+
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(selectQuery);
+            while (rs.next()) {
+                ads.add(
+                        new Ad(
+                                rs.getLong("id"),
+                                rs.getLong("user_id"),
+                                rs.getString("title"),
+                                rs.getString("description")
+                        ));
+            }
+            return ads;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error Fetching all Ads!", e);
+        }
+
     }
 
     @Override
     public Long insert(Ad ad) {
-
-        String query = "INSERT INTO ads(user_id, title, descritption) VALUES(";
+        String query = "INSERT INTO ads(user_id, title, description) VALUES(";
         query += "1, ";
-        query += "'" + ad.getTitle() + "'";
-        query += "'" + ad.getDescription() + "'";
-        query += ")";
-        System.out.println("[DEBUG]: Running SQL..." + query);
-
-        try{
+        query += "'" + ad.getTitle() + "', ";
+        query += "'" + ad.getDescription() + "')";
+        try {
             Statement stmt = connection.createStatement();
             stmt.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
             ResultSet rs = stmt.getGeneratedKeys();
-            rs.next();
-            if(rs.next()){
+            if (rs.next()) {
                 return rs.getLong(1);
             } else {
-                throw new RuntimeException()
+                throw new RuntimeException("No id found after inserting ad!");
             }
         } catch (SQLException e) {
-            throw new RuntimeException("error inserting an ad!", e);
+            throw new RuntimeException("Error inserting an ad!");
         }
-
-
     }
 }
