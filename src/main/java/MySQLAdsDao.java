d6ef9fd4b6be8c7755522f3b0e1ed6a36183a990
Art <art_jr2000@yahoo.com>
2017-10-23 11:56:59 -0500

    updated classes

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,6 +1,30 @@
+import com.mysql.jdbc.Driver;
+import java.sql.*;
 import java.util.List;
+import java.util.ArrayList;
 
 public class MySQLAdsDao implements Ads {
+    Connection connection = null;
+
+    public MySQLAdsDao (){
+        try {
+            DriverManager.registerDriver(new Driver());
+            Connection connection = DriverManager.getConnection(
+//                    "jdbc:mysql://localhost/movies_db",
+//                    "movies_user",
+//                    "password"
+                    Config.getUrl(),
+                    Config.getUser(),
+                    Config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error");
+        }
+    }
+
+    public MySQLAdsDao(Config config) {
+    }
+
     @Override
     public List<Ad> all() {
         return null;
@@ -8,6 +32,28 @@ public class MySQLAdsDao implements Ads {
 
     @Override
     public Long insert(Ad ad) {
-        return null;
+
+        String query = "INSERT INTO ads(user_id, title, descritption) VALUES(";
+        query += "1, ";
+        query += "'" + ad.getTitle() + "'";
+        query += "'" + ad.getDescription() + "'";
+        query += ")";
+        System.out.println("[DEBUG]: Running SQL..." + query);
+
+        try{
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            if(rs.next()){
+                return rs.getLong(1);
+            } else {
+                throw new RuntimeException()
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("error inserting an ad!", e);
+        }
+
+
     }
 }
